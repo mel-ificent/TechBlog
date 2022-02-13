4be8d68ec4c95a21838bedbe0167cb08f6f415db
Narcisah <91091964+mel-ificent@users.noreply.github.com>
2022-02-13 15:27:10 -0500

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 
 let sequelize;
 
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL);
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
